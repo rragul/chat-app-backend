d520fa10f2bd972088f397d636119022e3628865
rragul <r.ragul0598@gmail.com>
2023-01-03 13:41:07 +0530

    update origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://rragul.github.io/chat-app-frontend/",
         methods: ["GET", "POST"],
     },
 });
